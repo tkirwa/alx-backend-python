211558efef8bb08380b610429ea976f0693feea3
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-05 22:42:10 +0300

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Integer variable 'a' with a value of 1
+a: int = 1
+
+# Float variable 'pi' with a value of 3.14
+pi: float = 3.14
+
+# Boolean variable 'i_understand_annotations' with a value of True
+i_understand_annotations: bool = True
+
+# String variable 'school' with a value of "Holberton"
+school: str = "Holberton"
