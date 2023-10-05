e21fd38b07a6bac7e873564eb70d40a28887aedb
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-05 22:31:53 +0300

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result as a string.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
