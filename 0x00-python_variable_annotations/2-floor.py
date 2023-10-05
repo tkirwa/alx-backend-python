3a44f6d296f08be827e6bcda8a63e04ef2981226
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-05 22:35:22 +0300

    2-floor.py - Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math  # Import the math module to use math.floor
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number as an integer.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        int: The floor of the input number as an integer.
+    """
+    return math.floor(n)
