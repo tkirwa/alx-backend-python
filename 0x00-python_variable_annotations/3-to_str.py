0a447ed04e02991baf7150211d9686c4ffd8bd43
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-05 22:38:04 +0300

    3-to_str.py - Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a floating-point number.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        str: The string representation of the input number.
+    """
+    return str(n)
