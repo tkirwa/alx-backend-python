6fee67dc9d7493786966de9b2c27c27ef4f44e6b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-10 12:43:47 +0300

    0-async_generator.py - 0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -19,14 +19,3 @@ async def async_generator() -> AsyncGenerator[float, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-
-# Testing the async_generator coroutine
-if __name__ == "__main__":
-
-    async def print_yielded_values():
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-    asyncio.run(print_yielded_values())
