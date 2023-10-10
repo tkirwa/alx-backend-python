65648fa5b2adca62238b29bed85f735218c82b02
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-10 12:41:02 +0300

    0-async_generator.py - 0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -18,8 +18,7 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
+        yield random.random() * 10
 
 # Testing the async_generator coroutine
 if __name__ == "__main__":
