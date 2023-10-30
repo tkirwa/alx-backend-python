af15503c9ac22850cff5fc39f814ebf2e5706c85
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-30 17:38:38 +0300

    test_client.py - mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,17 +3,14 @@
 "test_client.py - Parameterize and patch as decorators"
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    @parameterized.expand([
-        ("google"),
-        ("abc")
-    ])
-    @patch('client.get_json', return_value={"payload": True})
+    @parameterized.expand([("google"), ("abc")])
+    @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org_name, mock_get_json):
         """
         Test function for `GithubOrgClient.org`. It asserts if the function
@@ -25,3 +22,19 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class_instance = GithubOrgClient(org_name)
         self.assertEqual(test_class_instance.org, {"payload": True})
         mock_get_json.assert_called_once()
+
+    @patch.object(GithubOrgClient, "org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        Test function for `GithubOrgClient._public_repos_url`. It asserts if
+          the function returns
+        the expected output for given inputs.
+        """
+
+        json_payload = {"repos_url": "World"}
+        mock_org.return_value = json_payload
+
+        test_class_instance = GithubOrgClient("test")
+        self.assertEqual(
+            test_class_instance._public_repos_url, json_payload["repos_url"]
+        )
