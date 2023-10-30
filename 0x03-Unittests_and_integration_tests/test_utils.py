4bc5006de48bf5b50d63452f845c3a48bf5c0501
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-30 17:31:53 +0300

    test_client.py - parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -124,22 +124,3 @@ class TestMemoize(unittest.TestCase):
             test_class.a_property
             test_class.a_property
             mock.assert_called_once()
-
-
-class TestGithubOrgClient(unittest.TestCase):
-    @parameterized.expand([
-        ("google"),
-        ("abc")
-    ])
-    @patch('client.get_json', return_value={"payload": True})
-    def test_org(self, org_name, mock_get_json):
-        """
-        Test function for `GithubOrgClient.org`. It asserts if the function
-          returns
-        the expected output for given inputs and checks if the decorated method
-        is called only once.
-        """
-
-        test_class_instance = GithubOrgClient(org_name)
-        self.assertEqual(test_class_instance.org, {"payload": True})
-        mock_get_json.assert_called_once()
