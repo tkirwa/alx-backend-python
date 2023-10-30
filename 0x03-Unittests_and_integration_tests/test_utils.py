edec53e427190362d93a572d68e86314616a0dac
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-30 15:57:31 +0300

    test_utils.py - Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"Parameterize a unit test"
+
+import unittest
+from parameterized import parameterized
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    This class inherits from `unittest.TestCase` and contains unit test for the
+    `utils.access_nested_map` function.
+    """
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """
+        Test function for `utils.access_nested_map`. It asserts if the
+          function returns
+        the expected output for given inputs. The test cases are provided
+          by the
+        `parameterized.expand` decorator.
+
+        Args:
+            nested_map (dict): The nested map to be tested.
+            path (tuple): The path to the value in the map.
+            expected_output (Any): The expected result from the function.
+        """
+        self.assertEqual(utils.access_nested_map(nested_map, path),
+                         expected_output)
